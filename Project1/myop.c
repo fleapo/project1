09c83a5cffd3216096c28ccda95e624e8b0d6167
fleapo@yeah.net <fleapo@yeah.net>
2016-04-18 17:10:31 +0800

    哈哈

---
+++ Project1/myop.c
@@ -0,0 +1,9 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+void main()
+{
+	int a = 1;
+	printf("123");
+}        
--- Project1/myop.h
+++ Project1/myop.h
@@ -1,1 +1,12 @@
 #pragma once
+typedef struct _Teacher
+{
+	char name[64];
+	int age;
+	char studentInfop[1024];
+
+}Teacher;
+
+int AddTeacher(char *pfileName, Teacher *pTe);
+
+int ModifyTeacher(char *pfileName, Teacher *pTe);
